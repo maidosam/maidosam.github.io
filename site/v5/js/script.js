00cbd00d5734dd97aa462862a1c15cdc6468d4ca
maidosam <sam@Maidos-MacBook-Pro-2.local>
2017-04-11 17:18:01 +0100

    Update

--- site/v5/js/script.js
+++
@@ -1,8 +0,0 @@
-$(window).on('load', function() {
-
-	$(".test-btn").on('click', function() {
-		$("#about").toggleClass('slide-in');
-		$("#main").toggleClass('slide-in');
-	});
-
-});
--- site/v8/js/script.js
+++
@@ -1,1 +0,0 @@
-window.scrollTo(500%, 500);
